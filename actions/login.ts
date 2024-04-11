8f1fda60bc357139d59ac7723d27990baf8aa649
Phi Nguyen <quangphi1231@gmail.com>
2024-04-11 21:43:58 +0700

    fix type again

--- actions/login.ts
+++ actions/login.ts
@@ -21,6 +21,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       password,
       redirectTo: DEFAULT_LOGIN_REDIRECT
     })
+
+    return { success: "Logged in!" }
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,6 +2,7 @@ import NextAuth, { type DefaultSession } from "next-auth"
 import type { UserRole } from "@prisma/client"
 
 export type ExtendedUser = DefaultSession["user"] & {
+  // id: string -- This is already defined in DefaultSession["user"]
   role: UserRole
 }
 
