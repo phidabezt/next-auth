59660c64a3a53a736e909004f1770775dcf5143d
ext-phi.nguyen <ext-phi.nguyen@spdigital.sg>
2024-04-11 17:39:20 +0700

    update user role type

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import { db } from "@/lib/db"
 import authConfig from "@/auth.config"
+import { getUserById } from "./data/user"
 
 export const {
   handlers: { GET, POST },
@@ -10,7 +11,30 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role
+      }
+
+      return session
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token
+
+      console.log("existingUser", existingUser)
+      token.role = existingUser.role
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
-  session: {strategy: "jwt"},
+  session: { strategy: "jwt" },
   ...authConfig,
 })
