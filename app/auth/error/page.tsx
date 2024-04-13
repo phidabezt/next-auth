629d00af5d7f649ea436282cd9090bf0ed26d63a
Phi Nguyen <quangphi1231@gmail.com>
2024-04-13 13:15:14 +0700

    resolve EOF

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,9 +1,7 @@
 import { ErrorCard } from "@/components/auth/ErrorCard"
 
 const AuthErrorPage = () => {
-  return (
-    <ErrorCard />
-  )
+  return <ErrorCard />
 }
 
 export default AuthErrorPage
